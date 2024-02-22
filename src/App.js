276712f26c8750c378543cbda6cd7e6bf2f3b308
ravi-relangi <relangi.ravi96@gmail.com>
2024-02-22 19:35:41 +0530

    [Add] Responsive layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import LoginComponent from './Containers/Login/LoginComponent';
-import './App.css';
-import Sidebar from './Containers/SideBar/Sidebar';
+// import LoginComponent from './Containers/Login/LoginComponent';
+// import Sidebar from './Containers/SideBar/Sidebar';
 
+import './App.css';
+import CustomLayout from './Containers/CustomLayout/CustomLayout';
 function App() {
   return (
     <div className='App'>
-      <div className='sideBar'>
+      {/* <div className='sideBar'>
         <Sidebar />
       </div>
       <div className='loginComponent'>
         <LoginComponent />
-      </div>
+      </div> */}
+      <CustomLayout />
     </div>
   );
 }
---
+++ src/Containers/CustomLayout/CustomLayout.js
@@ -0,0 +1,16 @@
+import './style.css';
+export default function CustomLayout() {
+  return (
+    <div className='customLayoutContainer'>
+      <div className='customLayoutLeftSection'></div>
+      <div className='customLayoutRightSection'>
+        <div className='card card1'></div>
+        <div className='card card2'></div>
+        <div className='card card3'></div>
+        <div className='card card4'></div>
+        <div className='card card5'></div>
+        <div className='card card6'></div>
+      </div>
+    </div>
+  );
+}
